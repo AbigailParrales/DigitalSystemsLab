584b293c7e9b8c9293c9723eb577a3e17804cc2b
AbigailParrales <danielaparralesd.m@gmail.com>
2017-08-08 11:00:52 -0500

    Creating the pulse gen

---
+++ clkpulsegen.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:00:24 08/08/2017 
+// Design Name: 
+// Module Name:    clkpulsegen 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module clkpulsegen(
+    input clk,
+    input rst,
+    output pulse
+    );
+
+
+endmodule
